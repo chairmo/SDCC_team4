d3da6a7a24ae410cdfb21778a21e5c2eed934f97
Sive <sisandla@student.wethinkcode.co.za>
2022-08-26 04:35:55 +0200

    minor fixes

--- src/components/views/About.js
+++ src/components/views/About.js
@@ -11,10 +11,10 @@ const About = () => {
                 <Col xs={8} className="mt-4">
                     <h1 className="display-4">About the Project</h1>
                     <hr style={{ width: "10rem" }} />
-                    <p className="text-left mt-3">Crowdfunding scheme
+                    <p className="text-left mt-3">Single Deadline Crowdfunding scheme
                   </p>
                
-                    <h2 className="text-left">Hello </h2>
+                    <h2 className="text-left">Description </h2>
                     <p className="text-left mt-3">
                     There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
 
